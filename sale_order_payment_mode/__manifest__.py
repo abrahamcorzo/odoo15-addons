7a18d405970dde1d40bf107636beed2c59b95271
abrahamcorzo <abrahamcorzo28@gmail.com>
2022-02-09 10:21:24 -0400

    Primer commit

---
+++ sale_order_payment_mode/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Modo de pago en Sale Order",
+
+    'summary': """
+       Módulo de prueba para el curso de Odoo 15
+    """,
+
+    'description': """
+       Módulo de prueba para el curso de Odoo 15
+    """,
+
+    'author': "Abraham Corzo Castillo",
+    'website': "https://ve.linkedin.com/in/abrahamcorzo",
+
+    'category': 'Tools',
+    'version': '0.1',
+
+    'depends': ['sale'],
+
+    'data': [
+       'views\sale_order_view.xml'
+    ]
+    
+}
